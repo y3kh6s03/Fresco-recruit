5c25940b6d92f6a0087c480f7e6fdaec257e9752
yosuke026 <y3kh6s03@gmail.com>
2023-08-02 18:09:04 +0900

    hero-completed

---
+++ src/app/components/Scroll/Scroll.jsx
@@ -0,0 +1,9 @@
+import "src/app/components/Scroll/Scroll.scss"
+
+export const Scroll = () => {
+    return (
+        <div className="scroll">
+            <span>SCROLL</span>
+        </div>
+    )
+}
