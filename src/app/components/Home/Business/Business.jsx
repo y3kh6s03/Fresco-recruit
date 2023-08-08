8ccb21a3383d70a783a5d5dbeab17e0597913926
yosuke026 <y3kh6s03@gmail.com>
2023-08-08 13:23:48 +0900

    fix

--- src/app/components/Home/Business/Business.jsx
+++ src/app/components/Home/Business/Business.jsx
@@ -7,18 +7,16 @@ export const Business = () => {
     return (
         <section className="business container">
             <h3 className="section__title">事業について</h3>
-            <div className="business__img__container">
-                <Image src="/images/home/mailchimp-Vc2dD4l57og-unsplash.jpg" fill alt="business-image"></Image>
-            </div>
             <div className="business__inner">
                 <h4 className="business__inner__title">
                     Lorem ipsum dolor sit amet.
                 </h4>
-                <div className="business__inner__content"> Maxime architecto sequi quia autem! Nobis blanditiis vero quo voluptatem assumenda ex.
+                <div className="business__inner__content">
+                    事業展開の方針を記載します。事業展開の方針を記載します。事業展開の方針を記載します。事業展開の方針を記載します。事業展開の方針を記載します。
                 </div>
                 <Button></Button>
             </div>
-            <div className="business__img__container2">
+            <div className="business__img__container">
                 <Image src="/images/home/mailchimp-Vc2dD4l57og-unsplash.jpg" fill alt="business-image"></Image>
             </div>
         </section>
