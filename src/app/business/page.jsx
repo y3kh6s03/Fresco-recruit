1286d60cceb41bb765b76945e9ab7dffab621462
yosuke026 <y3kh6s03@gmail.com>
2023-08-13 12:58:49 +0900

    business-create

--- src/app/business/page.jsx
+++ src/app/business/page.jsx
@@ -1,8 +1,14 @@
+import { Contents } from "@/app/business/components/Contents/Contents";
 import { Hero } from "@/app/business/components/Hero/Hero";
+
+import contentItems from "@/app/business/components/Contents/Contents.json"
 
 const Business = () => {
     return (
-        <Hero></Hero>
+        <>
+            <Hero></Hero>
+            <Contents contentItems={contentItems}></Contents>
+        </>
     )
 }
 
