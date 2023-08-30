e1323ad976a06174c0fd39ca290a876781b14de4
yosuke026 <y3kh6s03@gmail.com>
2023-08-30 16:35:11 +0900

    job_animation

--- src/app/jobs/Jobs.jsx
+++ src/app/jobs/Jobs.jsx
@@ -1,3 +1,4 @@
+import { ScrollAnimation, ScrollXAnimation } from "@/app/components/ScrollAnimation/ScrollAnimation";
 import Image from "next/image"
 
 import "src/app/jobs/Jobs.scss";
@@ -10,34 +11,41 @@ export const Jobs = ({ jobItems }) => {
                 jobItems.map((jobItem) => {
                     return (
                         <section key={jobItems.name} className={`jobs ${jobItem.name.toLowerCase()}`} id={jobItem.name.toLowerCase()}>
-                            <h3 className="jobs__name">
-                                {jobItem.name}
-                                <span className="jobs__jaName">
-                                    {jobItem.jaName}
-                                </span>
-                            </h3>
+                            <ScrollAnimation>
+                                <h3 className="jobs__name">
+                                    {jobItem.name}
+                                    <span className="jobs__jaName">
+                                        {jobItem.jaName}
+                                    </span>
+                                </h3>
+                            </ScrollAnimation>
 
                             <div className="jobs__wrapper">
                                 <div className="jobs__image">
                                     <Image src={jobItem.image} fill sizes="100%" alt="jobs-image"></Image>
                                 </div>
                                 <div className="jobs__inner">
-                                    <div className="jobs__text">
-                                        <h3 className="jobs__text__headline">
-                                            {jobItem.headLine1}
-                                        </h3>
-                                        <p className="jobs__text__description">
-                                            {jobItem.description1}
-                                        </p>
-                                    </div>
-                                    <div className="jobs__text2">
-                                        <h3 className="jobs__text2__headline">
-                                            {jobItem.headLine2}
-                                        </h3>
-                                        <p className="jobs__text2__description">
-                                            {jobItem.description2}
-                                        </p>
-                                    </div>
+                                    <ScrollXAnimation>
+                                        <div className="jobs__text">
+                                            <h3 className="jobs__text__headline">
+                                                {jobItem.headLine1}
+                                            </h3>
+                                            <p className="jobs__text__description">
+                                                {jobItem.description1}
+                                            </p>
+                                        </div>
+                                    </ScrollXAnimation>
+                                    <ScrollXAnimation>
+
+                                        <div className="jobs__text2">
+                                            <h3 className="jobs__text2__headline">
+                                                {jobItem.headLine2}
+                                            </h3>
+                                            <p className="jobs__text2__description">
+                                                {jobItem.description2}
+                                            </p>
+                                        </div>
+                                    </ScrollXAnimation>
                                 </div>
                             </div>
                         </section>
